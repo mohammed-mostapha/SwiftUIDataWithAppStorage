707af758138ba01fb7a19cb5ee3e28fdf443bbe9
mohamed mostapha <mohamed.mostapha3699@gmail.com>
2023-09-18 02:30:41 +0300

    Initial Commit

---
+++ UsersOnboarding/UsersOnboardingApp.swift
@@ -0,0 +1,17 @@
+//
+//  UsersOnboardingApp.swift
+//  UsersOnboarding
+//
+//  Created by Mohamed Mostapha on 18/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct UsersOnboardingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
